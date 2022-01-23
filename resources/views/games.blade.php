3c25257cbbddad6d83e45c56759760bfe6c994e2
Davide Tavella <tavella01@gmail.com>
2022-01-23 19:55:22 +0100

    link pagine header

---
+++ resources/views/games.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   <title>Games DC Comics</title>
+</head>
+<body>
+
+   <h1>Games DC Comics</h1>
+
+   <p
+   style="margin-bottom: 15px; color:black">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis necessitatibus, dolorem laboriosam praesentium perferendis quas odit aut totam, perspiciatis, iusto distinctio facilis tempore! Nulla eaque itaque adipisci qui ex. Et aspernatur assumenda facilis eius itaque illo deserunt officia provident! Dolores suscipit consectetur ipsa eum facere, deleniti obcaecati molestiae mollitia? Labore corrupti minima ipsum ad tempora molestias, voluptate, nisi itaque asperiores cumque consectetur. Harum illum omnis quisquam, commodi itaque rerum sit reiciendis molestiae iste quibusdam sequi facere nisi facilis quidem amet dolore eum enim odio esse quae magni. Provident eligendi, delectus facilis iusto ullam quo praesentium rem ducimus rerum dolorum amet!
+   </p>
+
+   <a
+   style="color:blue" 
+   href="/">Torna alla Home</a>
+   
+</body>
+</html>
